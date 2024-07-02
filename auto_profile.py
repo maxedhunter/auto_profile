107324332d04dbe70ec4975814746f162e837f3e
Maxwell He <u6059299@notchpeak1.int.chpc.utah.edu>
2024-07-02 14:27:48 -0600

    generalized auto_profile

--- auto_profile.py
+++ auto_profile.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 import shutil
 import os
 from simple_slurm import Slurm
@@ -17,29 +18,34 @@ class AutoProfile:
     
     def run(self):
         replicates, values = self.config['runs']['replicates'], self.config['runs']['values']
-        for replicate in range(replicates):
-            for value in values:
+        for value in values:
+            for replicate in range(1, replicates + 1):
                 self.run_replicate(replicate, value)
     
     def run_replicate(self, replicate, value):
-        path = os.path.join(self.config['files']['root_dir'], f"{value}_{replicate}")
+        template_name = os.path.basename(os.path.normpath(self.template_dir))
+        path = os.path.join(self.config['files']['root_dir'], f"{template_name}_nstep{value}_r{replicate}")
         shutil.copytree(self.template_dir, path)
 
-        input_file_path = os.path.join(path, self.config['files']['input_file_relative_to_template'])
-        search_term = self.config['files']['input_search_term']
+        config_file_path = os.path.join(path, self.config['files']['config_file_relative_to_template'])
+        search_term = self.config['files']['config_search_term']
 
-        self.update_value(value, input_file_path, search_term)
+        self.update_value(value, config_file_path, search_term)
         self.run_slurm(replicate, value, path)
 
-    def update_value(self, value, input_file_path, search_term):
-        if os.path.exists(input_file_path):
-            with open(input_file_path, 'w') as f:
+    def update_value(self, value, config_file_path, search_term):
+        if os.path.exists(config_file_path):
+            with open(config_file_path, 'r+') as f:
                 lines = f.readlines()
                 for i, line in enumerate(lines):
                     if search_term in line:
                         lines[i] = f"{search_term} = {value}\n"
+                
+                f.seek(0)
+                f.writelines(lines)
+                f.truncate()
         else:
-            raise FileNotFoundError(f"File {input_file_path} not found")
+            raise FileNotFoundError(f"File {config_file_path} not found")
 
     def run_slurm(self, replicate, value, path):
         # runs profile, copies the .prof file over after finishing
@@ -53,15 +59,15 @@ class AutoProfile:
 
         variables = {
             "profile_name":f"nstep{value}_r{replicate}.prof",
-            "script_module":os.path.join(path, self.config['files']['script_module_relative_to_template']),
-            "input_file":os.path.join(self.config['files']['input_file_relative_to_template']),
+            "script_module":self.config['files']['script_module_relative_to_template'],
+            "input_file":self.config['files']['input_file_relative_to_template'],
             "output_file":f"nstep{value}_r{replicate}.out", 
             "outputs":self.config['files']['outputs'],
             "working_dir":path,
             "conda_env":self.config['conda_env'],
         }
-
-        commands = self.config.pop('slurm_commands', [])
+        print(variables)
+        commands = self.config.get('slurm_commands', [])
         print(commands)
         for cmd in commands:
             try:
@@ -73,6 +79,7 @@ class AutoProfile:
                 raise TypeError(f"Error formatting command '{cmd}': {e}")
 
         print(slurm)
+        slurm.sbatch(shell="/bin/bash")
 
     @staticmethod
     def path_to_module(filepath):
@@ -80,6 +87,9 @@ class AutoProfile:
         return module_name
 
 if __name__ == "__main__":
-    auto_profile = AutoProfile()
-    auto_profile.run_slurm(0, 0, "/test/dir")
-    # auto_profile.run()
+    parser = argparse.ArgumentParser(description="Profiling json config file")
+    parser.add_argument("config_file", help="Path to json file for automatic profiling")
+
+    args = parser.parse_args()
+    auto_profile = AutoProfile(args.config_file)
+    auto_profile.run()
